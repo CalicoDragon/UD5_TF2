ed05c1cdef9f6fb11275c9909719741007aa2a97
Sofia Gilsanz <softie.gd@gmail.com>
2025-03-23 16:43:39 +0100

    finished questions

--- questions.js
+++ questions.js
@@ -12,14 +12,159 @@ export const questions = [
     ],
   },
   {
-    text: "ayo?",
+    text: "What is more important to you?",
     answers: [
-      { text: "Explosives, aye!", mercenary: ["soldier", "demoman"] },
-      { text: "Mhhhm!! (fire!)", mercenary: ["pyro"] },
-      { text: "Neither!", mercenary: ["heavy", "sniper", "medic"] },
+      { text: "Movement!", mercenary: ["scout", "soldier", "demoman"] },
+      { text: "Helping my team!", mercenary: ["engineer", "pyro", "medic"] },
       {
-        text: "I hate them with my very soul...",
-        mercenary: ["spy", "scout", "engineer"],
+        text: "Getting essential kills!",
+        mercenary: ["heavy", "sniper", "spy"],
+      },
+    ],
+  },
+  {
+    text: "Are you a furry?",
+    answers: [
+      { text: "Mhmmm!", mercenary: ["pyro"] },
+      { text: "No!!", mercenary: ["scout", "sniper", "heavy", "engineer"] },
+      {
+        text: "What is that?",
+        mercenary: ["soldier", "demoman", "medic", "spy"],
+      },
+    ],
+  },
+  {
+    text: "What is your favourite taste?",
+    answers: [
+      { text: "Mmmhmm! (Sweet)", mercenary: ["pyro"] },
+      { text: "Salty", mercenary: ["scout", "heavy"] },
+      {
+        text: "Sour",
+        mercenary: ["sniper"],
+      },
+      {
+        text: "Bitter",
+        mercenary: ["demoman", "medic"],
+      },
+      {
+        text: "Spicy",
+        mercenary: ["engineer", "soldier"],
+      },
+      {
+        text: "Cigarettes",
+        mercenary: ["spy"],
+      },
+    ],
+  },
+  {
+    text: "Spyyy!!!!, What do you do?",
+    answers: [
+      { text: "Attack everywhere!", mercenary: ["pyro", "soldier", "demoman"] },
+      { text: "I ignore it", mercenary: ["scout"] },
+      {
+        text: "Hope it is not too late!",
+        mercenary: ["sniper", "engineer", "medic", "heavy"],
+      },
+      {
+        text: "Fuck",
+        mercenary: ["spy"],
+      },
+    ],
+  },
+  {
+    text: "Quickly! Do a fornite dance!",
+    answers: [
+      { text: "The Floss!", mercenary: ["scout", "pyro"] },
+      { text: "I do the L one", mercenary: ["sniper", "demoman"] },
+      {
+        text: "Uhhh, I will just do the Kazotsky Kick",
+        mercenary: ["soldier", "medic", "heavy"],
+      },
+      {
+        text: "I'm not dancing",
+        mercenary: ["spy", "engineer"],
+      },
+    ],
+  },
+  {
+    text: "What DnD class are you?",
+    answers: [
+      { text: "Fighter!", mercenary: ["soldier", "heavy"] },
+      { text: "Cleric!", mercenary: ["medic", "pyro"] },
+      {
+        text: "Ranger!",
+        mercenary: ["sniper"],
+      },
+      {
+        text: "Rogue!",
+        mercenary: ["spy", "scout"],
+      },
+      {
+        text: "Wizard!",
+        mercenary: ["engineer", "demoman"],
+      },
+    ],
+  },
+  {
+    text: "Pick a country to visit!",
+    answers: [
+      { text: "USA", mercenary: ["scout", "soldier", "engineer"] },
+      { text: "Pyroland", mercenary: ["pyro"] },
+      {
+        text: "Scotland",
+        mercenary: ["demoman"],
+      },
+      {
+        text: "Russia",
+        mercenary: ["heavy"],
+      },
+      {
+        text: "Germany",
+        mercenary: ["medic"],
+      },
+      {
+        text: "Australia",
+        mercenary: ["sniper"],
+      },
+      {
+        text: "France",
+        mercenary: ["spy"],
+      },
+    ],
+  },
+  {
+    text: "Do you have a hobby?",
+    answers: [
+      { text: "Reading", mercenary: ["heavy"] },
+      { text: "Science", mercenary: ["engineer", "medic"] },
+      {
+        text: "Murder, of course!",
+        mercenary: ["soldier", "spy", "sniper"],
+      },
+      {
+        text: "Running",
+        mercenary: ["scout"],
+      },
+      {
+        text: "Drinking",
+        mercenary: ["demoman"],
+      },
+      {
+        text: "Mhmmm! (you don't wanna know)",
+        mercenary: ["pyro"],
+      },
+    ],
+  },
+  {
+    text: "Were you even able to read this quiz?",
+    answers: [
+      {
+        text: "Yes! Who do you think I am!",
+        mercenary: ["heavy", "engineer", "medic", "spy", "pyro"],
+      },
+      {
+        text: "Uhmmm... 50/50 I guess",
+        mercenary: ["demoman", "soldier", "scout", "sniper"],
       },
     ],
   },
